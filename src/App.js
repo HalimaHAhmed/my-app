02f59a41ed6ee9cee1e7c5bf68bc2903702e8db4
halimaHussein <hanahussein6155.com>
2023-02-28 12:34:19 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* this page will be like menu pages  */}
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
