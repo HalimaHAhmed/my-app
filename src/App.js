f0a37f45571253fad55c5e402a282c6e5813bd38
HalimaHAhmed <hanahussein6155@gmail.com>
2023-02-28 14:03:40 +0300

    added bundle bootstrap

--- src/App.js
+++ src/App.js
@@ -19,4 +19,6 @@ function App() {
   );
 }
 
+pages 
+
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { NavLink, link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className="header-top-strip">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="col-6"></div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
