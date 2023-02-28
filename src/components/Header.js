7323859277345443552d6262f44b31585a388793
HalimaHAhmed <hanahussein6155@gmail.com>
2023-02-28 16:01:56 +0300

    headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import { NavLink,link } from 'react-router-dom'
+import React from "react";
+import { NavLink, link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-    <header className='header-top-strip'>
-
-      <div className='container-xxl'>
-        <div className='row'>
-          <div className='col-6'>
-            <div className='col-6'>
-              
+      <header className="header-top-strip py-3 ">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="color">Free Shopping Over $100 & Free returns</p>
+            </div>
+            <div className="col-6">
+              <p className="text">
+                Hotline: <a href="tel:252 8978656">+252 8976676</a>
+              </p>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
